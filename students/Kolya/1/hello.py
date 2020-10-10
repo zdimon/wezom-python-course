5cfd3b3fcf2d3eaa65d6b07adcf60183180cb38b
Your Name <kolyakhozhajinov@gmail.com>
2020-10-10 13:41:49 +0300

    init 4

--- students/Kolya/1/hello.py
+++ students/Kolya/1/hello.py
@@ -1,1 +1,9 @@
-print('Hello World')
+print('Start')
+print('Какой ваш вес?')
+a = int(input())
+if a <= 30:
+	print('Вы худой')
+elif a > 60:
+	print('Вы толстый')
+else:
+	print('Вы нормального веса')
