395b80e1f91bba13792fcd615438eccea6810084
Your Name <kolyakhozhajinov@gmail.com>
2020-10-10 13:55:15 +0300

    init 4

--- students/Kolya/1/hello.py
+++ students/Kolya/1/hello.py
@@ -1,9 +1,19 @@
+'''
 print('Start')
 print('Какой ваш вес?')
 a = int(input())
 if a <= 30:
 	print('Вы худой')
 elif a > 60:
-	print('Вы толстый')
-else:
+	print('Вы полный')
+elif a > 30 and a <= 60: 
 	print('Вы нормального веса')
+'''
+import random
+while True:
+	k = random.randint(1, 10)
+	a = int(input('Введите число от 1 до 10: '))
+	if a == k:
+		break
+	else:
+		print('Неправильно')
