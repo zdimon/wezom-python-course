75da09cc62ae1fb6c0e163922ec08a6d0b3b4b7a
Your Name <kolyakhozhajinov@gmail.com>
2020-10-14 19:34:30 +0300

    init 4

--- students/Kolya/2/string.py
+++ students/Kolya/2/string.py
@@ -1,9 +1,27 @@
+
 f = open('data.txt', 'r')
 string = f.read()
 t = []
-for row in string.split(';'):
-	a = input(row)
-	t.append(a)
-print(' ')
-for row in t:
-	print(row)
+s = ';'
+i = 1
+for row in string.split(':'):
+	k = i
+	for row2 in row.split('\n'):
+		if i == k:
+			t.append(str(i) + ' - ' + row2)
+			i = i + 1
+		else:
+			t.append(str(i) + ' - ' + row2)
+			i = i + 1
+print(t)
+#for row in t:
+#	f = open(row + '.txt', 'w')
+'''
+pr = []
+s = '-'
+for row in range(0, 101):
+	pr.append(str(row))
+s = s.join(pr)
+print(s)
+
+'''
