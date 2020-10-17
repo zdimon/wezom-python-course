eb9107f2ce3c542fb18e6bbe26fdf7687918e73b
Your Name <you@example.com>
2020-10-17 19:43:24 +0300

    n

--- students/maks/4/sys.py
+++ students/maks/4/sys.py
@@ -1,11 +1,10 @@
-import sys
+#import sys
 #import os
 #sys.argv
 
 
-#for i in sys.argv:
-	#k = i[0:]
-	#print(k)
+#for i in sys.argv[1:]:
+	#print(i)
 
 
 
@@ -20,8 +19,8 @@ import sys
 
 
 
-a = open("test.txt", "r")
-for line in a.readlines():
+#a = open("test.txt", "r")
+#for line in a.readlines():
 	#b = line.find(sys.argv[1])
 	
 	#if b > -1:
@@ -29,10 +28,44 @@ for line in a.readlines():
 		#p = line.replace("can", "cant")
 		#print(p)
 
-	v = line.find(sys.argv[1])
-	if v > -1:
-		l = line.replace(sys.argv[1], sys.argv[2])
-		print(l)
+	#v = line.find(sys.argv[1])
+	#if v > -1:
+		#l = line.replace(sys.argv[1], sys.argv[2])
+		#print(l)
 
 #txt = a.read()
 #print(txt)
+
+
+
+import sys
+
+summa_correct = 0
+summa_wrong = 0
+
+a = input("Укажите ваше имя: ")
+print(a + ", пройдите небольшой опрос")
+
+with open(a + ".txt", "w") as que:
+	que1 = input("What is 4+4?: ")
+	if que1 == "8":
+		summa_correct += 1
+	else:
+		summa_wrong += 1
+
+	que2 = input("What is 3*3?: ")
+	if que2 == "9":
+		summa_correct += 1
+	else:
+		summa_wrong += 1
+
+	que3 = input("What is 7-3?: ")
+	if que3 == "4":
+		summa_correct += 1
+	else:
+		summa_wrong += 1
+
+	que.write("Количество правильных ответов: " + str(summa_correct))
+	que.write("\nКоличество неправильных ответов: " + str(summa_wrong))
+
+print("Спасибо что прошли опрос!")
