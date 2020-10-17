540fcff5528345bafd05838ed498ad63aee908c1
Your Name <you@example.com>
2020-10-17 14:13:58 +0300

    f

--- students/maks/4/sys.py
+++ students/maks/4/sys.py
@@ -1,10 +1,12 @@
 import sys
-import os
+#import os
 #sys.argv
+
 
 #for i in sys.argv:
 	#k = i[0:]
 	#print(k)
+
 
 
 #with open("this", "w") as o:
@@ -17,8 +19,20 @@ import os
 #print("All good")
 
 
-a = open("blue", "r")
-for line in f.readLines():
-	print(line)
+
+a = open("test.txt", "r")
+for line in a.readlines():
+	#b = line.find(sys.argv[1])
+	
+	#if b > -1:
+		#print(line)
+		#p = line.replace("can", "cant")
+		#print(p)
+
+	v = line.find(sys.argv[1])
+	if v > -1:
+		l = line.replace(sys.argv[1], sys.argv[2])
+		print(l)
+
 #txt = a.read()
 #print(txt)
