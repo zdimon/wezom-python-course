e2372ab9718dd406156cd385502063d1bf2cbfec
Dmitry Zharikov <zdimon77@gmail.com>
2020-10-21 19:42:40 +0300

    commit from zdimon77@gmail.com ----

---
+++ teacher/5/mylib.py
@@ -0,0 +1,3 @@
+from os import listdir
+def mylistdir(dirname):
+    return listdir(dirname)
--- teacher/5/work.py
+++ teacher/5/work.py
@@ -1,7 +1,30 @@
-from pack.lib import mylist
-from pack.lib2.test import DOMAIN
+# from bs4 import BeautifulSoup
+
+# import requests
+
+# txt = requests.get('https://pythondigest.ru/')
+
+# soup = BeautifulSoup(txt.text, 'html.parser')
+
+# links = soup.find_all('a', {"class": "issue-item-title"})
+
+# for link in links:
+#     print(link.text)
+
+# l = [1,2,3,4]
+#md = { 'books': [1,2,3], 'users': [{ 'name': 'Dima' }] }
+
+# e = {"key": [1,2,3,4]}
+# e['key'][1]
+
+import json
+
+f = open('quiz.json', 'r')
+mystr = f.read()
+data = json.loads(mystr)
+for item in data:
+    print(item['question'])
 
 
-print('Start program with %s' % DOMAIN)
-rezult = mylist(4,5)
-print(rezult)
+
+
