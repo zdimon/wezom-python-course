2414fbca9f388bd193957c799b8041cdf3b1e4f8
Your Name <kolyakhozhajinov@gmail.com>
2020-10-24 13:18:22 +0300

    first commit

---
+++ students/Kolya/6/tbot.py
@@ -0,0 +1,35 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, CallbackContext
+from telegram import Bot
+
+
+
+print('Привет я спящий бот и я проснулся!')
+from utils import get_data
+data = get_data()
+print(data)
+bot = Bot(token=data['ключ'])
+
+def start(update: Updater, context: CallbackContext):
+    print("Start command!")
+    print(update.message.from_user['username'])
+    room_id = update.message.chat_id
+    print(room_id)
+    bot.send_message(room_id,'Hello. How are you?')
+
+
+start_handler = CommandHandler('start', start)
+
+updater = Updater(token=data['ключ'], use_context=True)
+updater.dispatcher.add_handler(start_handler)
+updater.start_polling()  
+
+
+
+
+
+
+
+
+
+
--- students/Kolya/6/tg.py
+++ students/Kolya/6/tg.py
@@ -1,11 +1,25 @@
-import os.path
-if not os.path.isfile('secret.txt'):
-	name = input('Введите имя бота: ')
-	token = input('Введите код: ')
-	f = open('secret.txt', 'w')
-	f.write(name + ':' + token)
-else:
-	f = open('secret.txt', 'r')
-	mystr = f.read()
-	for row in mystr.split(':'):
-		print(row)
+#1346530025:AAHtDsnGKbnpZi3EW6bu8tGyd_YPCE_NyQg
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, CallbackContext
+from telegram import Bot
+import json
+f = open('question.json', 'r')
+txt = f.read()
+a = json.loads(txt)
+from get_data import get_data
+data = get_data()
+bot = Bot(token=data['код'])
+
+def start(update: Updater, context: CallbackContext):
+    room_id = update.message.chat_id
+    bot.send_message(room_id,a[0]['question']%update.message.from_user.username)
+
+
+start_handler = CommandHandler('start', start)
+
+updater = Updater(token=data['код'], use_context=True)
+updater.dispatcher.add_handler(start_handler)
+updater.start_polling()  
+
+
+
