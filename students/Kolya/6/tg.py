6d98cc40ded247d9e1926113079051f603fdb336
kol-oss <62239358+kol-oss@users.noreply.github.com>
2020-10-24 13:53:51 +0300

    Update tg.py

--- students/Kolya/6/tg.py
+++ students/Kolya/6/tg.py
@@ -1,8 +1,10 @@
+#@Fffffgfsffbot
 import telebot
-bot = telebot.TeleBot('1346530025:AAHtDsnGKbnpZi3EW6bu8tGyd_YPCE_NyQg')
+bot = telebot.TeleBot('1288611759:AAEKGMZM53kZTN7jbAYir1a_dGuEXUS0P_A')
 @bot.message_handler(commands=['start'])
 def start(message):
 	bot.send_message(message.chat.id, 'Привет')
 bot.polling()
+
 
 
