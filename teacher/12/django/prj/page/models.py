5c7b3edb7048716ec848c208f540cd25e23a6fcc
Dmitry Zharikov <zdimon77@gmail.com>
2020-11-21 14:27:00 +0200

    commit from zdimon77@gmail.com ----

--- teacher/12/django/prj/page/models.py
+++ teacher/12/django/prj/page/models.py
@@ -18,3 +18,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Basket(models.Model):
+    name = models.CharField(max_length=250)
+    phone = models.TextField()
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, default='', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
